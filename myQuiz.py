4c41d586079dec5a4dc65b5e617e6bd258cc0447
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-07-22 16:19:09 -0700

    Python

---
+++ myQuiz.py
@@ -0,0 +1,9 @@
+
+import Budget
+
+# Option 1: Call the function directly after import
+print(Budget.calcBills())
+
+# Option 2: Import the function directly and then call it
+from Budget import calcBills
+print(calcBills())
---
+++ testCode1.py
@@ -0,0 +1,14 @@
+# This is my note.
+
+""" Note
+still note
+and again still note
+"""
+
+def printMe():
+    """this is my docstring
+    """
+
+    print("Meeeeeee!")
+
+print(printMe.__doc__)
